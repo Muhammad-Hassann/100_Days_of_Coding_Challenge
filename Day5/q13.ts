88cac81abe7e95a5a9ff6e1a74ac862fc2008341
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-03-10 00:55:39 +0500

    Adding Day5 Challange

--- Day5/q13.ts
+++ Day5/q13.ts
@@ -1,8 +1,14 @@
-let persons : string[] = ["Hammad", "Waqas", "Owais"]
-let invitation_message = ", You are invited to dinner today"
+/* Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”*/
 
-persons.forEach(element => {
-    console.log(element + invitation_message)
+let cars : string[] = ["Toyota", "Land Cruiser", "Tesla", "Ferrari"]
+
+let statement1 = "My favorite car is "
+let statement2 = "I would like to own a "
+let statement3 = "My dream car is "
+
+cars.forEach(element => {
+    console.log("\n" + statement1 + element)
+    console.log(statement2 + element)
+    console.log(statement3 + element) 
 });
-
-export{}
--- Day5/q14.ts
+++ Day5/q14.ts
@@ -1,14 +1,11 @@
-/* Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
-to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”*/
+// Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to
+// invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
 
-let cars : string[] = ["Toyota", "Land Cruiser", "Tesla", "Ferrari"]
+let persons : string[] = ["Hammad", "Waqas", "Owais"]
+let invitation_message = ", You are invited to dinner today"
 
-let statement1 = "My favorite car is "
-let statement2 = "I would like to own a "
-let statement3 = "My dream car is "
-
-cars.forEach(element => {
-    console.log("\n" + statement1 + element)
-    console.log(statement2 + element)
-    console.log(statement3 + element) 
+persons.forEach(element => {
+    console.log(element + invitation_message)
 });
+
+export{}
