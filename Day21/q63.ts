9f179bf564d82e447a8cec7b113627f2acb2057a
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-03-22 22:38:13 +0500

    day 20 tasks

---
+++ Day21/q63.ts
@@ -0,0 +1,1 @@
+// Making a Student Template: Create a blueprint for student information, including their name, age, and the classes they're taking, and then fill in this blueprint with an example student.`
