49f122b102ad49972beed61cd0c435d9df21ded4
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-03-23 04:46:59 +0500

    Completed tasks of day 20

--- Day21/q61.ts
+++ Day21/q61.ts
@@ -1,1 +1,9 @@
 // Question 61: Making Enums for Vehicles: Let's create categories for vehicles like cars, trucks, and motorcycles using enums, and show one example.
+
+const enum vehicles{
+     car,
+     truck,
+     motorcycle
+}
+
+console.log(vehicles.car)
