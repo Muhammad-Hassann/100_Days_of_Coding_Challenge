8385e91a3d462082e08bfeed64cb80b0fae692da
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-04-02 18:43:50 +0500

    Modify Q no. 87

--- Day29/q87.ts
+++ Day29/q87.ts
@@ -1,7 +1,7 @@
 // Question 87: Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
 
 function extract(sentence : string): string{
-    return sentence.substring(0, 10)
+    return sentence.substring(0, 9)
 }
 
-console.log("Hello World")
+console.log("Hello World again!")
