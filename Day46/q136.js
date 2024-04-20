d73d8cb153a70e9dd1a80ac6a0a31bbd0024a456
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-04-20 23:14:39 +0500

    Completed tasks of day 46

---
+++ Day46/q136.js
@@ -0,0 +1,4 @@
+// Question 136: Use console.log() to debug and track the value of a variable inside a loop.
+for (var i = 1; i <= 5; i++) {
+    console.log("Iteration ".concat(i, ", i value:, i")); // Logs the current iteration number and the value of i
+}
---
+++ Day46/q137.js
@@ -0,0 +1,7 @@
+// Question 137: Implement a try-catch block to handle potential errors in a block of code.
+try {
+    throw new Error("Something went wrong");
+}
+catch (error) {
+    console.log(error.message);
+}
