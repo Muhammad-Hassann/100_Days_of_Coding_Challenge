fba85b0fe2dce4a04c1c8eb94f6cccef91100d78
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-04-26 02:41:25 +0500

    Adding day 55

---
+++ Day55/Data-Types/task.js
@@ -0,0 +1,8 @@
+// What is the Output of this code
+let name = "Ilya";
+
+alert( `hello ${1}` ); // Output will hello 1
+
+alert( `hello ${"name"}` ); //  hello name
+
+alert( `hello ${name}` ); // hello Ilya
