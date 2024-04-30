d390742dfb1d9ee6bcc6b9dc2b3eb0b1282d8cae
Muhammad Hassan <muhammadhassan202005@gmail.com>
2024-05-01 02:54:50 +0500

    Complete day 56

---
+++ Day56/q158.js
@@ -0,0 +1,9 @@
+// What are the final values of all variables a, b, c and d after the code below?
+
+let a = 1, b = 1;
+
+let c = ++a; // Output will be 2
+let d = b++; // Output will be 1
+
+// Now a = 2
+// b also = 2 
---
+++ Day56/q159.js
@@ -0,0 +1,9 @@
+// What are the values of a and x after the code below?
+
+let a = 2;
+
+let x = 1 + (a *= 2); 
+
+// Output will 
+// x = 5, because a = 2 and  a *= 2 becomes a = 4 and + 1 = 5
+// a = 4
